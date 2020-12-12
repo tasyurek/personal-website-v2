6d5c1ecb276e20f697d807fb6bb1aa7e7f7cff65
onurcantasyurek <onurcantasyurek@gmail.com>
2020-12-12 14:41:04 +0300

    fix ssl

--- server.js
+++ server.js
@@ -6,18 +6,9 @@ const mailgunRouter = require("./routers/mailgun");
 const enforce = require("express-sslify");
 
 const port = process.env.PORT || 8080;
-
-const https = require("https");
-const fs = require("fs");
-
-const options = {
-  key: fs.readFileSync(__dirname + "/ssl/key.pem", "utf8"),
-  cert: fs.readFileSync(__dirname + "/ssl/cert.pem", "utf8"),
-};
-
 const publicPath = path.join(__dirname, "client", "build");
 
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static(publicPath));
 app.use(weatherRouter);
 app.use(mailgunRouter);
@@ -26,13 +17,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-const server = https.createServer(options, app).listen(port);
-
-server.on("clientError", (err, socket) => {
-  console.error(err);
-  socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
-});
-
-// app.listen(port, () => console.log("server is runnig"));
+app.listen(port, () => console.log("server is runnig"));
 
 // asd
