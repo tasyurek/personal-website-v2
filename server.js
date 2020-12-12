f343692fd8f6211f15d6bfde53022662407507bc
onurcantasyurek <onurcantasyurek@gmail.com>
2020-12-12 12:08:50 +0300

    Fix SSL

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const enforce = require("express-sslify");
 
 const port = process.env.PORT || 8080;
 
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
 console.log(process.env.NODE_ENV);
 
 const publicPath = path.join(__dirname, "client", "build");
@@ -16,15 +24,16 @@ app.use(express.static(publicPath));
 app.use(weatherRouter);
 app.use(mailgunRouter);
 
-app.get("/api/test", (req, res) => {
-  console.log("api/test");
-  res.send("/api/test is working.");
-});
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(port, () => console.log("server is runnig"));
+https
+  .createServer(options, function (req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+  })
+  .listen(port);
+// app.listen(port, () => console.log("server is runnig"));
 
 // asd
